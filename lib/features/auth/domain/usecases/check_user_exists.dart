4b941bab9c654f7ecad9e875103a366d7e366ca4
Abdalluh <abdallesam100@gmail.com>
2025-09-15 02:52:40 +0300

    feat: refactor authentication and routing to support new login structure and improve dependency injection

---
+++ lib/features/auth/domain/usecases/check_user_exists.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import '../../../../core/errors/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../repositories/user_repository.dart';
+
+class CheckUserExistsUseCase implements UseCase<bool, CheckUserExistsParams> {
+  final UserRepository repository;
+
+  CheckUserExistsUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, bool>> call(CheckUserExistsParams params) async {
+    return await repository.checkUserExists(params.identifier);
+  }
+}
+
+class CheckUserExistsParams extends Equatable {
+  final String identifier;
+
+  const CheckUserExistsParams({required this.identifier});
+
+  @override
+  List<Object> get props => [identifier];
+}
